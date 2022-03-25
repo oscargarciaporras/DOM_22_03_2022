2d6be164ba70f676a28010eae3caad6ea3d5b83b
oscargarciaporras <osligarpo92@gmail.com>
2022-03-24 21:43:56 -0500

    Manipulación DOM StyleCajas, JS

--- main.js
+++ main.js
@@ -1,26 +1,35 @@
 
+let div = document.querySelector(".container");
+let divCaja2 = document.querySelector(".container > div:nth-child(2)");
 
-let selecionElento = document.querySelector("body > ul > li:nth-child(3)");
-console.log("1",selecionElento);
+// let Mystyle = {
+//     background: "orange",
+//     color: "#fff",
+//     "font-family": "'Roboto', sans-serif"
+// };
 
-let selecionAtributos = document.querySelector("#Mylista > li:nth-child(3)");
-console.log("2", selecionAtributos);
+// Object.assign(divConteiner.style, Mystyle);
 
-let numero = 14523;
-let selecionDataset = document.querySelectorAll("#Mylista > li");
-selecionDataset.forEach(element => {
-    if(element.dataset.cc != undefined){
-        if(element.dataset.cc.includes(numero) && numero != null){
-            console.log("3", element);
-        }else if(numero==null){
-            console.log("3", element);
-        }
-        // let res = (element.dataset.cc.includes(numero) && numero != null) 
-        //     ? ["3", element]
-        //     :((numero==null) ? ["3", element] : "")
-        // console.log(...res);
-    }
-});
+divCaja2.classList.add('Mystyle');
+
+
+// let Mystyle3 = {
+//     margin: "auto",
+//     top: 0,
+//     left: 0,
+//     right: 0,
+//     bottom: 0,
+//     "font-family": "'Inspiration', cursive",
+//     "font-size": "35px",
+//     color: "blueviolet",
+//     "font-weight": 800,
+//     background: "#2be286"
+// }
+let divCaja3 = document.createElement("DIV");
+divCaja3.insertAdjacentText("beforeend", "Circulo 3");
+// Object.assign(divCaja3.style, Mystyle3);
+
+div.insertAdjacentElement("beforeend", divCaja3);
 
 
 
